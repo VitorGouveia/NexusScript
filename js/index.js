1dc38e84abc4488ea8f44d9bfdd6a6d62f4b1322
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-19 16:00:38 -0300

    feat: add features section

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
+const $ = document.querySelector.bind(document)
+
+const arrowDown = document.querySelector(".arrowDown")
+
+feather.replace()
+
 document.addEventListener("DOMContentLoaded", () => {
   if("serviceWorker" in navigator) {
     navigator.serviceWorker.register("../pwa/sw.js")
@@ -5,4 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     console.log("Registro de Service Worker falhou")
   }
+
+  setInterval(() => {
+    arrowDown.classList.toggle("go-down")
+  }, 1200)
 })
