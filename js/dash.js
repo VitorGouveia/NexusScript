a0175460a771a170691090241df931e0c63fcb5e
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-20 07:40:15 -0300

    feat: add settings modal

--- js/dash.js
+++ js/dash.js
@@ -1,9 +1,22 @@
 const $ = document.querySelector.bind(document)
 
 const settingsButton = document.querySelector("#settings")
+const settingsModal = document.querySelector(".settings-modal")
+const settingsModalCancel = document.querySelector('.settings-modal button[data-action="cancel"]')
+const settingsModalConfirm = document.querySelector('.settings-modal button[data-action="confirm"]')
 
 document.addEventListener("DOMContentLoaded", () => {
   settingsButton.addEventListener("click", () => {
     console.log("open settings!")
+
+    settingsModal.classList.remove("popOut")
+    settingsModal.classList.add("popIn")
+    settingsModal.classList.add("active")
+  })
+
+  settingsModalCancel.addEventListener("click", () => {
+    settingsModal.classList.add("popOut")
+    settingsModal.classList.remove("popIn")
+    settingsModal.classList.remove("active")
   })
 })
