f6baf3831de05b9945e675364267546a12058b8f
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-09-20 06:42:12 -0300

    fest: add full page transitions

---
+++ js/transition.js
@@ -0,0 +1,13 @@
+const links = document.querySelectorAll("a")
+
+document.body.classList.add("fadeIn")
+links.forEach(link => {
+  link.addEventListener("click", event => {
+    event.preventDefault()
+    
+    document.body.classList.remove("fadeIn")
+    document.body.classList.add("fadeOut")
+    
+    window.location.href = link.hrefgit 
+  })
+})
